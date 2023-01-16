8413a455b104cb9f17431e740ad9007a538072d8
Djamahl Etchegaray <djamahl.etchegaray@data61.csiro.au>
2023-01-16 12:14:16 +1000

    cleanup eval and dataset

--- dataset/kaggle_aims_pair_dataset_w_boxes.py
+++ dataset/kaggle_aims_pair_dataset_w_boxes.py
@@ -13,6 +13,29 @@ import numpy as np
 
 import torchvision.transforms as T
 
+def process_ann_box(ann: dict, image: dict, size: tuple):
+    # rescale the box to the current size
+    old_size = (image['height'], image['width'])
+    ratios = (size[0]/old_size[0], size[1]/old_size[1])
+
+    # coco 2 xyxy
+    bbox = np.array(ann['bbox'])
+    [xy1, wh] = np.split(bbox, (2))
+    xy2 = xy1 + wh
+    bbox = np.concatenate((xy1, xy2), axis=-1)
+
+    # apply ratio
+    bbox[0::2] *= ratios[1]
+    bbox[1::2] *= ratios[0]
+
+    # clip box
+    bbox[0::2] = np.clip(bbox[0::2], 0.0, float(size[1]))
+    bbox[1::2] = np.clip(bbox[1::2], 0.0, float(size[0]))
+
+    assert bbox[0::2].max() <= size[1] and bbox[1::2].max() <= size[0], f"bbox OOB after rescale? {bbox} {size} {old_size} {ratios}"
+
+    return bbox
+
 class kaggle_aims_pair_boxed(data.Dataset):
     def __init__(self, aims_split="train.json") -> None:
         super().__init__()
@@ -24,7 +47,7 @@ class kaggle_aims_pair_boxed(data.Dataset):
         self.size = (768, 1280)
         # self.size = (1088, 1920)
         
-        # add more later
+        # add more later TODO: use mmyolo LetterResize ... transforms
         self.transform_kaggle = T.Compose(transforms=[
             #T.CenterCrop(1080),
             T.Resize(self.size),
@@ -47,7 +70,7 @@ class kaggle_aims_pair_boxed(data.Dataset):
 
         # self.aims_root = "/home/etc004/code/YOLOX/AIMS_data_test"
         # self.aims_root = "/mnt/d61-visage-data/work/datasets/"
-        self.aims_root = "../AIMS_data_test"
+        self.aims_root = "AIMS_data_test"
 
         # just use train :)
         self.kaggle_anns = str(Path(self.kaggle_root) / "annotations/mmdet_split_train.json")
@@ -61,9 +84,11 @@ class kaggle_aims_pair_boxed(data.Dataset):
             aims_data = json.load(f)
 
         kaggle_image_id_to_image = {x['id']: x for x in kaggle_data['images']}
+        aims_image_id_to_image = {x['id']: x for x in aims_data['images']}
 
         kaggle_imgs_labels = {}
         kaggle_imgs_boxes = {}
+        aims_imgs_boxes = {}
         aims_imgs_labels = {}
 
         cots_cat_id_kaggle = 0
@@ -73,6 +98,14 @@ class kaggle_aims_pair_boxed(data.Dataset):
             if ann['category_id'] == cots_cat_id_aims:
                 aims_imgs_labels[ann['image_id']] = 1.0
 
+                if not ann['image_id'] in aims_imgs_boxes:
+                    aims_imgs_boxes[ann['image_id']] = {'bboxes': [], 'labels': []}
+
+                bbox = process_ann_box(ann, aims_image_id_to_image[ann['image_id']], self.size)
+
+                aims_imgs_boxes[ann['image_id']]['bboxes'].append(bbox)
+                aims_imgs_boxes[ann['image_id']]['labels'].append(1)
+
         for ann in tqdm(kaggle_data['annotations']):
             if ann['category_id'] == cots_cat_id_kaggle:
                 kaggle_imgs_labels[ann['image_id']] = 1.0
@@ -80,27 +113,7 @@ class kaggle_aims_pair_boxed(data.Dataset):
                 if not ann['image_id'] in kaggle_imgs_boxes:
                     kaggle_imgs_boxes[ann['image_id']] = {'bboxes': [], 'labels': []}
 
-                # rescale the box to the current size
-                image = kaggle_image_id_to_image[ann['image_id']]
-                old_size = (image['height'], image['width'])
-                ratios = (self.size[0]/old_size[0], self.size[1]/old_size[1])
-
-                # coco 2 xyxy
-                bbox = np.array(ann['bbox'])
-                [xy1, wh] = np.split(bbox, (2))
-                xy2 = xy1 + wh
-                bbox = np.concatenate((xy1, xy2), axis=-1)
-
-                # print('bbox before rescale', bbox)
-
-                bbox[0::2] *= ratios[1]
-                bbox[1::2] *= ratios[0]
-    
-                bbox[0::2] = np.clip(bbox[0::2], 0.0, float(self.size[1]))
-                bbox[1::2] = np.clip(bbox[1::2], 0.0, float(self.size[0]))
-                # print('bbox after rescale', bbox)
-
-                assert bbox[0::2].max() <= self.size[1] and bbox[1::2].max() <= self.size[0], f"bbox OOB after rescale? {bbox} {self.size} {old_size} {ratios}"
+                bbox = process_ann_box(ann, kaggle_image_id_to_image[ann['image_id']], self.size)
 
                 kaggle_imgs_boxes[ann['image_id']]['bboxes'].append(bbox)
                 kaggle_imgs_boxes[ann['image_id']]['labels'].append(1)
@@ -111,16 +124,17 @@ class kaggle_aims_pair_boxed(data.Dataset):
             'image_id': x['id'], 
             'file_name': self.aims_root + "/" + x['file_name'],
             'label': 0.0 if x['id'] not in aims_imgs_labels else 1.0
-            } for x in tqdm(aims_data['images'][0:ms])]
+        } for x in tqdm(aims_data['images'][0:ms])]
 
         self.kaggle_imgs_list = [{
             'image_id': x['id'], 
             'file_name': self.kaggle_root + "/" + x['file_name'],
             'label': 0.0 if x['id'] not in kaggle_imgs_labels else 1.0,
             'boxes': {'bboxes': [], 'labels': []} if x['id'] not in kaggle_imgs_boxes else kaggle_imgs_boxes[x['id']]
-            } for x in tqdm(kaggle_data['images'][0:ms])]
+        } for x in tqdm(kaggle_data['images'][0:ms])]
 
         self.kaggle_imgs_boxes = kaggle_imgs_boxes
+        self.aims_imgs_boxes = aims_imgs_boxes
 
         num_with_boxes = 0
         num_without = 0
@@ -145,8 +159,6 @@ class kaggle_aims_pair_boxed(data.Dataset):
 
         print("number of images", len(self.aims_imgs_list), len(self.kaggle_imgs_list))
 
-        # self.shortest_list = 10000 # TODO
-
         assert self.shortest_list > 0, "shortest dataset should have more than 0 existing images!"
 
     def __len__(self):
@@ -156,7 +168,6 @@ class kaggle_aims_pair_boxed(data.Dataset):
 
         # go though all aims split (evaluating yolo on aims val/test)
         return len(self.aims_imgs_list)
-        # return self.shortest_list
 
     def __getitem__(self, index) -> tuple:
         if self.aims_split == "train.json":
